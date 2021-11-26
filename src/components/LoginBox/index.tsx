d0dc83d940c58c05c08bf34afc6c70bda9bf580a
Matheus Massari <massari.matheus@gmail.com>
2021-11-26 09:00:18 -0300

    message form under development

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,45 +1,13 @@
-import { useEffect } from "react";
+import { useContext } from "react";
 import { VscGithubInverted } from "react-icons/vsc";
-import { api } from "../../services/api";
+import { AuthContext } from "../../contexts/auth";
 import styles from "./styles.module.scss";
 
-type AuthResponse = {
-    token: string;
-    user: {
-        id: string;
-        avatar_url: string;
-        name: string;
-        login: string;
-    }
-}
 
 const LoginBox = () => {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=2090616bc31d70cca79e`;
+  const {signInUrl} = useContext(AuthContext)
 
-  useEffect(() => {
-    const url = window.location.href;
-    const hasGithubCode = url.includes("?code=");
-    if(hasGithubCode) {
-        const [urlWithoutCode, githubCode] = url.split('?code=')
-        window.history.pushState({},'', urlWithoutCode)
 
-        signIn(githubCode)
-    }
-
-  }, []);
-
-  const signIn = async (githubCode:string) => {
-    const response = await api.post<AuthResponse>('authenticate', {
-        code: githubCode
-    })
-
-    
-    const {token, user} = response.data;
-    
-    localStorage.setItem('@dowhile:token', token)
-
-    console.log(user)
-  }
 
   return (
     <div className={styles.loginBoxWrapper}>
---
+++ src/components/SendMessageForm/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss'
+
+export function SendMessageForm() {
+    return (
+        <h1>SendMessageForm</h1>
+    )
+}
